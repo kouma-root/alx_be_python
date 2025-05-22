8b14419cdbfd8273b6c869e6c5538287da7cbf4b
kouma-root <parfaitkoumakang@gmail.com>
2025-05-22 08:42:03 +0100

    My first python program for basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+# Sum   
+sum_result = number1 + number2
+#print the sum
+print ("Sum of ", number1, "and", number2, "is:", sum_result)
+# Subtraction
+sub_result = number1 - number2
+#print the subtraction
+print ("Difference of ", number1, "and", number2, "is:", sub_result)
+# Multiplication
+mul_result = number1 * number2
+#print the multiplication
+print ("Product of ", number1, "and", number2, "is:", mul_result)
