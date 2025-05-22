81df99d85816674db8c6f11331fdc8d55ab5b110
kouma-root <parfaitkoumakang@gmail.com>
2025-05-22 09:06:25 +0100

    Changes on file simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000
 rate = 0.05
 time = 3
-SI = principal * rate * time
-print("The Simple Interest is:", SI)
+interest = principal * rate * time
+print("The Simple Interest is:", interest)
