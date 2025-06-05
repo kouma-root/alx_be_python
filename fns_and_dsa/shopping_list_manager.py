ce19c755d09019918c4c5918731c7b47d02e5324
kouma-root <parfaitkoumakang@gmail.com>
2025-06-05 23:09:01 +0100

    Modify input value in the  file for shopping management

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            adding = input("What do you want to add: ")
+            adding = input("Enter the item to add: ")
             shopping_list.append(adding)
             pass
         elif choice == '2':
