1a1d0baacb3e0b34b7372344b170105a4205e2c4
kouma-root <parfaitkoumakang@gmail.com>
2025-06-08 08:33:48 +0100

    Adding new function for calculate date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,13 +1,13 @@
-import datetime
+from datetime import datetime
 
 
 def display_current_datetime() :
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 
 def calculate_future_date(days) :
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     future_date = current_date + datetime.timedelta(days=days)
 
     print(f"Future date: ",future_date.strftime("%Y-%m-%d"))
