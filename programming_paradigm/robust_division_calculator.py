df3122e22e02806b108b5eb214b0133f78a0fc44
kouma-root <parfaitkoumakang@gmail.com>
2025-06-15 01:25:53 +0100

    Adding files for exception Handling project

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,8 +2,8 @@ def safe_divide(numerator, denominator):
     try :
         result = (float(numerator) / float(denominator))
     except ZeroDivisionError as e :
-        return("Error: Cannot divide by zero")
+        return("Error: Cannot divide by zero.")
     except ValueError as e :
-        return("Error: Please enter numeric values only")
+        return("Error: Please enter numeric values only.")
     else :
         return(f"The result of the division is {result}")
