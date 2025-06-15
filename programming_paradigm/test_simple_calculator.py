e5a49c161ebb5c22562a07fcbae174fc5a1f6de3
kouma-root <parfaitkoumakang@gmail.com>
2025-06-15 01:50:07 +0100

    Adding file for Unit_test Project

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,25 @@
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0) 
+    def test_substract(self):
+        self.assertEqual(self.calc.subtract(3,4),-1)
+        self.assertEqual(self.calc.subtract(6,4),2)
+        
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3,4),12)
+        self.assertEqual(self.calc.multiply(6,4),24)
+        
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(6,2),3)
+        self.assertRaises( ZeroDivisionError, self.calc.divide,6,0)
