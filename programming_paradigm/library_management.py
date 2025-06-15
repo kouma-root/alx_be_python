e99ce49b8186fcac646efe6d761880ac8868e913
kouma-root <parfaitkoumakang@gmail.com>
2025-06-15 02:57:30 +0100

    Adding files for th Library Management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -15,22 +15,22 @@ class Book:
 
 class Library:
     def __init__(self):
-        self._book = []
+        self._books = []
         self._checked_out_books = []
     def add_book(self, book):
         if isinstance(book, Book):
-            self._book.append(book)
+            self._books.append(book)
         else:
             raise TypeError("Only Book instances can be added to the library.")
     def list_available_books(self):
-        available_books = [book for book in self._book if book._Book__is_checked_out == 0]
+        available_books = [book for book in self._books if book._Book__is_checked_out == 0]
         if not available_books:
             print("No books available.")
         else:
             for book in available_books:
                 print(f"{book.title} by {book.author}")
     def check_out_book(self, title):
-        for book in self._book:
+        for book in self._books:
             if book.title == title and book._Book__is_checked_out == 0:
                 book._Book__is_checked_out = 1
                 self._checked_out_books.append(book)
