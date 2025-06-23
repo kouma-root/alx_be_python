be49e1d478d7df7a4f2da62af791c4293908e978
kouma-root <parfaitkoumakang@gmail.com>
2025-06-23 18:30:36 +0100

    Adding files for the polymorphism demo in the oop directory

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+import math
+
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must needed")
+    
+    
+class Rectangle(Shape):
+    def __init__(self,length,width) -> None:
+        self.length = length
+        self.width = width
+        
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius) -> None:
+        self.radius = radius
+        
+    def area(self):
+        return math.pi * self.radius
+    
